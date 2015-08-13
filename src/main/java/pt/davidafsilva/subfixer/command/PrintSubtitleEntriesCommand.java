9a2f7981d8c2f0f76cb727f0538a0ab6e150e459
David Silva <david@davidafsilva.pt>
2015-08-13 15:57:41 +0100

    used UTF-8 as output encoding rather than the configured encoding for the input file loading

--- src/main/java/pt/davidafsilva/subfixer/command/PrintSubtitleEntriesCommand.java
+++ src/main/java/pt/davidafsilva/subfixer/command/PrintSubtitleEntriesCommand.java
@@ -12,10 +12,10 @@ package pt.davidafsilva.subfixer.command;
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -34,7 +34,7 @@ import java.util.stream.IntStream;
 import java.util.List;
 import pt.davidafsilva.subfixer.load.SubtitleEntry;
 
-import static pt.davidafsilva.subfixer.config.Configuration.CHARSET;
+//import static pt.davidafsilva.subfixer.config.Configuration.CHARSET;
 
 /**
  * This command prints the subtitle entries to a designated output stream.
@@ -70,7 +70,7 @@ public final class PrintSubtitleEntriesCommand
                                     entry.getEndTime().toString(),
                                     entry.getText());
              })
-             .map(str -> str.getBytes(CHARSET))
+             .map(str -> str.getBytes(StandardCharsets.UTF_8))
              .forEach(this::write);
     // return them as is
     return entries;
